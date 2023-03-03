c646c0db02b799acce6749512a1fd6baa95f11e7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2023-03-03 13:26:48 +0100

    Added a custom image quality generator

---
+++ src/Package/code/DecoratedImageUrlGenerator.cs
@@ -0,0 +1,29 @@
+// <copyright file="DecoratedImageUrlGenerator.cs" company="Umbraco Community">
+// Copyright (c) Dave Woestenborghs and contributors
+// </copyright>
+
+using Umbraco.Cms.Core.Media;
+using Umbraco.Cms.Core.Models;
+
+namespace Umbraco.Community.ImageQualityReducer
+{
+    /// <summary>
+    /// A decorated <see cref="IImageUrlGenerator"/> that allows us to append the image quality querystring if needed.
+    /// </summary>
+    internal sealed class DecoratedImageUrlGenerator: IImageUrlGenerator
+    {
+        private IImageUrlGenerator innerGenerator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DecoratedImageUrlGenerator"/> class.
+        /// </summary>
+        /// <param name="innerGenerator">The default <see cref="IImageUrlGenerator"/>.</param>
+        public DecoratedImageUrlGenerator(IImageUrlGenerator innerGenerator) => this.innerGenerator = innerGenerator;
+
+        /// <inheritdoc/>
+        public string? GetImageUrl(ImageUrlGenerationOptions options) => this.innerGenerator.GetImageUrl(options);
+
+        /// <inheritdoc/>
+        public IEnumerable<string> SupportedImageFileTypes => this.innerGenerator.SupportedImageFileTypes;
+    }
+}
